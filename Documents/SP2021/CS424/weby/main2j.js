b1feef128e6f9253a2acdbb96e815a44deac1199
welcome <me294llamaxoption@ME294LSDs-MacBook-Pro-2.local>
2021-03-22 09:36:51 -0500

    yu3

--- Documents/SP2021/CS424/weby/main2j.js
+++ Documents/SP2021/CS424/weby/main2j.js
@@ -27,3 +27,109 @@ function go_add(ev){
   selector.append(box_item); 
 
 }
+
+
+function go_delete(ev){
+   
+    var kui = (ev.id).split('_');
+    var a  = document.querySelector("#popup_"+kui[1]);
+    a.style.visibility ="hidden";
+    a.classList.remove("show"); 
+}; 
+
+
+function go_detail(ev){
+    var id = ev.id;
+    var kui = id.split('_');
+ 
+
+    var pop  = document.querySelector("#popup_"+kui[1]);
+    var pop_img = pop.firstElementChild;
+   
+    pop_img.style.width = "300px";
+    pop_img.style.height = "200px";
+    
+    var food_item = document.querySelector(".food_item"); 
+   
+    pop.style.width =  (food_item.offsetWidth).toString()+"px";
+    
+    if(pop.style.visibility == "hidden" || pop.style.visibility==""){
+        console.log("in hidden");
+        pop.style.visibility ="visible";
+        pop.classList.add("show"); 
+
+     
+    }
+    
+   
+   
+    //console.log(pop_content.children.length);
+    //console.log(pop_content.children[1].id);
+  
+
+    
+}
+
+var pa = document.querySelector(".food_item").children;
+console.log(pa[0].id);
+console.log(pa[1].id);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+ 1.show
+ 2. no show 
+ 
+ 1.no show
+ 2.show -- no have show()
+*/
+
+/*
+$("#px").toggle(function(){
+    console.log("sdsd");
+    a.classList.add("show");
+    a.textContent = "hellow" //overwrite
+    
+    console.log(a.classList.length);
+}
+
+);       */
+
+/*
+console.log(pop_content.firstElementChild.id);
+var co  = pop_content.firstElementChild; 
+var co1 = co.nextElementSibling;
+console.log(co1.id);
+
+child = pop_content.children.length // give 1, is pic 
+
+ pop_content.childNodes.length // is 2, such  pic, content
+
+ var first  =pop_content.firstChild // work
+    var s = first.nextSibling;
+    console.log(s.id);
+
+
+var first  =pop_content.firstElementChild // not work
+    var s = first.nextElementSibling;
+    console.log(s.id);
+
+*/
